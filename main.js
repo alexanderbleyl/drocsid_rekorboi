7759c34fb00f2c4bec070ca2c1bce54e2cc0596f
Alexander Bleyl <ab@burgeins.de>
2021-10-28 15:50:38 +0200

    * try other main.js

--- main.js
+++ main.js
@@ -3,82 +3,95 @@
 const utils = require('@iobroker/adapter-core');
 const Discord = require('discord.js');
 
+/**
+ * The adapter instance
+ * @type {ioBroker.Adapter}
+ */
+let adapter;
+
 let client = null;
 
-class DiscordBot extends utils.Adapter {
-    /**
-     * @param {Partial<utils.AdapterOptions>} [options={}]
-     */
-    constructor(options) {
-        super({
-            ...options,
-            name: 'discord_bot',
-        });
-        this.on('ready', this.onReady.bind(this));
-        this.on('stateChange', this.onStateChange.bind(this));
-        this.on('message', this.message.bind(this));
-    }
-
-    /**
-     * Is called when databases are connected and adapter received configuration.
-     */
-    async onReady() {
-        if(this.config.bot_token === '') {
-            this.log.error('No Bot-Token given!');
-            return;
-        }
-        client = new Discord.Client();
-        client.login(this.config.bot_token);
-        if(this.config.state === '') {
-            this.subscribeStates('sendMessage');
+function sendMessageToDiscord(message) {
+    return new Promise((resolve, reject) =>  {
+        if (message && adapter.config.channel_id) {
+            client.channels.cache.get(adapter.config.channel_id).send(message);
+            resolve();
         } else {
-            this.subscribeForeignStates(this.config.state);
+            reject();
         }
-    }
-
-    /**
-     * Is called if a subscribed state changes
-     * @param {object} obj
-     */
-    message(obj) {
-        this.log.info('we receive a message from e.g. blockly');
-        this.log.info(JSON.stringify(obj));
-        if (typeof obj === 'object' && obj.message && this.config.channel_id) {
-            if (obj.command === 'send' ) {
-                // e.g. send email or pushover or whatever
-                this.log.info(`Send "${obj.message.text}"`);
-                client.channels.cache.get(this.config.channel_id).send(obj.message);
-            }
-        }
-    }
-
-    /**
-     * Is called if a subscribed state changes
-     * @param {string} id
-     * @param {ioBroker.State | null | undefined} state
-     */
-    onStateChange(id, state) {
-        if (state && state.val != '') {
-            try {
-                client.channels.cache.get(this.config.channel_id).send(state.val);
-            } catch (e) {
-                this.log.info(`could not send message "${state.val}"`);
-            }
-        } else {
-            // The state was deleted
-            this.log.info(`state ${id} deleted or value empty`);
-        }
-    }
-
+    });
 }
 
-if (require.main !== module) {
-    // Export the constructor in compact mode
-    /**
-     * @param {Partial<utils.AdapterOptions>} [options={}]
-     */
-    module.exports = (options) => new DiscordBot(options);
+/**
+ * Starts the adapter instance
+ * @param {Partial<utils.AdapterOptions>} [options]
+ */
+function startAdapter(options) {
+    // Create the adapter and define its methods
+    return adapter = utils.adapter(Object.assign({}, options, {
+            name: 'discord_bot',
+            ready: main,
+            stateChange: (id, state) => {
+                if (state && state.val != '') {
+                    try {
+                        sendMessageToDiscord(state.val);
+                    } catch (e) {
+                        adapter.log.info(`could not send message "${state.val}"`);
+                    }
+                } else {
+                    // The state was deleted
+                    adapter.log.info(`state ${id} deleted or value empty`);
+                }
+            },
+            message: (obj) => {
+                if (typeof obj === 'object' && obj.message) {
+                    if (obj.command === 'send' ) {
+                        if (typeof obj.message !== 'object') {
+                            obj.message = {
+                                text: obj.message
+                            };
+                        }
+            
+                        // e.g. send email or pushover or whatever
+                        adapter.log.info(`Send ${obj.message.text}`);
+            
+                        sendMessageToDiscord(obj.message.text)
+                            .then(() => {
+                                // Send response in callback if required
+                                obj.callback && adapter.sendTo(obj.from, obj.command, {result: 'Message sent'}, obj.callback);
+                            })
+                            .catch(err => {
+                                adapter.log.error('Cannot send message: ' + err);
+                                obj.callback && adapter.sendTo(obj.from, obj.command, {error: err}, obj.callback);
+                            });
+                    }
+                }
+            }
+        }));
+    }
+
+function main() {
+    if(!adapter.config.bot_token || adapter.config.bot_token === '') {
+        adapter.log.error('No Bot-Token given!');
+        return;
+    }
+    client = new Discord.Client();
+    client.login(adapter.config.bot_token);
+    if(adapter.config.state === '') {
+        adapter.subscribeStates('sendMessage');
+    } else {
+        adapter.subscribeForeignStates(adapter.config.state);
+    }
+    
+    // in this template all states changes inside the adapters namespace are subscribed
+    adapter.subscribeStates('sendMessage');
+}
+
+// @ts-ignore parent is a valid property on module
+if (module.parent) {
+    // Export startAdapter in compact mode
+    module.exports = startAdapter;
 } else {
     // otherwise start the instance directly
-    new DiscordBot();
+    startAdapter();
 }
